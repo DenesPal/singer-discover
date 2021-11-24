2c698ddbe2bb397c348b0743006ed96757349cc7
Dennis <43446428+DenesPal@users.noreply.github.com>
2021-11-24 15:02:59 +0100

    Inherit selected=true from input and option to sort
    If field was selected in input, or automatic, or selected-by-default, then output it as selected.
    
    Option -s/--sort to sort streams and fields alphabetically during selection and on outpot.

--- singer_discover/__init__.py
+++ singer_discover/__init__.py
@@ -2,8 +2,10 @@
 import os
 import sys
 import argparse
+import collections
 import json
 from singer import metadata, get_logger
+from singer.catalog import Catalog
 import tty
 
 from PyInquirer import prompt
@@ -20,6 +22,7 @@ def breadcrumb_name(breadcrumb):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--output', '-o', type=str, required=True)
+    parser.add_argument('--sort', '-s', action='store_true')
 
     if sys.stdin.isatty():
         parser.add_argument('--input', '-i', type=str, required=True)
@@ -38,23 +41,28 @@ def main():
         sys.stdin = sys.stdout
 
     logger.info("Catalog configuration starting...")
+    catalog = Catalog.from_dict(catalog)
+    streams = catalog.streams
+
+    if args.sort:
+        streams = sorted(streams, key=lambda s: s.stream)
 
     select_streams = {
         'type': 'checkbox',
         'message': 'Select Streams',
         'name': 'streams',
         'choices': [
-            {'name': stream['stream']} for stream in catalog['streams']
+            {'name': stream.stream, 'checked': stream.is_selected()} for stream in streams
         ]
     }
 
     selected_streams = prompt(select_streams)
 
-    for i, stream in enumerate(catalog['streams']):
+    for i, stream in enumerate(catalog.streams):
 
-        mdata = metadata.to_map(stream['metadata'])
+        mdata = metadata_to_map(stream.metadata)
 
-        if stream['stream'] not in selected_streams['streams']:
+        if stream.stream not in selected_streams['streams']:
             mdata = metadata.write(
                 mdata, (), 'selected', False
             )
@@ -67,16 +75,24 @@ def main():
 
             field_reference = {}
 
-            for breadcrumb, field in mdata.items():
+            keys = mdata.keys()
+
+            if args.sort:
+                keys = sorted(keys)
+
+            for breadcrumb in keys:
+                field = mdata[breadcrumb]
 
                 if breadcrumb != ():
                     selected, disabled = False, False
-                    if metadata.get(
-                            mdata, breadcrumb, 'inclusion') == 'automatic':
+                    
+                    if metadata.get(mdata, breadcrumb, 'inclusion') == 'automatic':
                         selected, disabled = True, "automatic"
 
-                    elif metadata.get(
-                            mdata, breadcrumb, 'selected-by-default'):
+                    elif metadata.get(mdata, breadcrumb, 'selected-by-default'):
+                        selected, disabled = True, False
+
+                    elif metadata.get(mdata, breadcrumb, 'selected'):
                         selected, disabled = True, False
 
                     name = breadcrumb_name(breadcrumb)
@@ -92,7 +108,7 @@ def main():
             stream_options = {
                 'type': 'checkbox',
                 'message': 'Select fields from stream: `{}`'.format(
-                    stream['stream']),
+                    stream.stream),
                 'name': 'fields',
                 'choices': fields
             }
@@ -106,24 +122,27 @@ def main():
 
             for breadcrumb in mdata.keys():
                 if breadcrumb != ():
-                    if (
-                        metadata.get(
-                            mdata, breadcrumb, 'inclusion') == "automatic"
-                    ) or (
-                        breadcrumb in selections
-                    ):
-                        mdata = metadata.write(
-                            mdata, breadcrumb, 'selected', True)
-                    else:
-                        mdata = metadata.write(
-                            mdata, breadcrumb, 'selected', False)
+                    if (metadata.get(mdata, breadcrumb, 'inclusion') == 'automatic'
+                            and metadata.get(mdata, breadcrumb, 'selected') is not None):
+                        metadata.delete(mdata, breadcrumb, 'selected')
+                    
+                    if breadcrumb in selections:
+                        mdata = metadata.write(mdata, breadcrumb, 'selected', True)
+                        
+                    elif (metadata.get(mdata, breadcrumb, 'selected-by-default') 
+                            or metadata.get(mdata, breadcrumb, 'selected')):
+                        mdata = metadata.write(mdata, breadcrumb, 'selected', False)
 
-            catalog['streams'][i]['metadata'] = metadata.to_list(mdata)
+            catalog.streams[i].metadata = metadata.to_list(mdata)
 
     logger.info("Catalog configuration saved.")
 
     with open(args.output, 'w') as f:
-        json.dump(catalog, f, indent=2)
+        json.dump(catalog.to_dict(), f, indent=2)
+
+
+def metadata_to_map(metadata):
+    return collections.OrderedDict((tuple(md['breadcrumb']), md['metadata']) for md in metadata)
 
 
 if __name__ == '__main__':
